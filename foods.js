c17f7a7fcd055877d01dccc35ee0e11b613a3dbc
kiannabifar-sys <kiannabifar@gmail.com>
2026-07-09 16:25:17 +0330

    foods.js

---
+++ foods.js
@@ -0,0 +1,39 @@
+const foods = [
+
+{
+name:"قیمه نثار",
+image:"images/gheymeh-nesar.jpg",
+description:"معروف‌ترین غذای سنتی قزوین که با برنج، گوشت، خلال بادام و پسته، زرشک و ادویه‌های معطر تهیه می‌شود."
+},
+
+{
+name:"دیماج",
+image:"images/dimaj.jpg",
+description:"غذای محلی و سبک قزوین که با نان خشک، پنیر، سبزی، گردو و پیازداغ تهیه می‌شود."
+},
+
+{
+name:"باقلوای قزوین",
+image:"images/baghlava.jpg",
+description:"یکی از معروف‌ترین شیرینی‌های سنتی قزوین با طعمی شیرین و عطر هل و گلاب."
+},
+
+{
+name:"نان چایی",
+image:"images/nan-chaii.jpg",
+description:"شیرینی سنتی محبوب قزوین که معمولاً همراه چای سرو می‌شود."
+},
+
+{
+name:"پادرازی",
+image:"images/paderazi.jpg",
+description:"شیرینی قدیمی و معروف قزوین با بافتی ترد و طعمی دلپذیر."
+},
+
+{
+name:"آش دوغ قزوین",
+image:"images/ash-doogh.jpg",
+description:"آش سنتی و خوش‌طعم که در برخی مناطق استان قزوین طبخ می‌شود."
+}
+
+];
